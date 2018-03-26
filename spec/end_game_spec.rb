d387ceb4d7d0563000312520a1ad76c20f87c52e
Devin Mork <devin.mork@gmail.com>
2018-03-26 06:42:08 -0500

    Time to ask for help.

--- spec/end_game_spec.rb
+++ spec/end_game_spec.rb
@@ -1,10 +1,21 @@
 require 'rspec'
 
-require 'end_game.rb'
+require_relative '../lib/end_game.rb'
+require_relative '../lib/board.rb'
 
 describe EndGame do
 
   # moc up pos_arr (position arrays)
+  let(:board) { Board.new }
+
+  describe '.get_endgame_combinations' do
+    context 'passed board.position_arr' do
+      it 'returns Array' do
+        expect(get_endgame_combinations(board.position_arr).class).to eql(Array)
+      end
+    end
+
+  end
   # pass position arrays into .get_endgame_combinations, confirm returns array
   # run get_ops and confirm proper array returns
   # run .victory? on all winning combinations
